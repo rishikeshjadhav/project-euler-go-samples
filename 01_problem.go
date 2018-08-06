3f006c811240f28113a2849278c422561675d845
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2018-08-06 17:31:52 +0530

    Solution for Problem 1

---
+++ 01_problem.go
@@ -0,0 +1,23 @@
+// Multiples of 3 and 5
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to Project Euler - Problem 1")
+
+	sum := 0
+	for i := 0; i < 1000; i++ {
+		// fmt.Println(i)
+
+		if i%3 == 0 || i%5 == 0 {
+			fmt.Println(i)
+			sum += i
+		}
+	}
+
+	fmt.Println(sum)
+}
